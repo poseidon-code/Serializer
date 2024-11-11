c01f8fe4de681115bf9dfc2e23b92ac45685fb71
Pritam Halder <61552390+poseidon-code@users.noreply.github.com>
2024-11-11 10:00:00 +0530

    update: license email

--- include/Serializer/Serializer.hpp
+++ include/Serializer/Serializer.hpp
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 Author : Pritam Halder
-Email : pritamhalder.portfolio@gmail.com
+Email : pritamhalder.poseidon@proton.me
 */
 
 #pragma once
